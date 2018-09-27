15d679afd1f52549c02014e16c9f92fd1ee0ba64
dcheu <19404312+dcheu@users.noreply.github.com>
2018-09-28 01:57:03 +1000

    Restructured with blueprints.

--- cstr/__init__.py
+++ cstr/__init__.py
@@ -1,19 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, current_app, Blueprint
+import os
 
-app = None
 
 def create_app(config=None):
     app = Flask(__name__)
+    app.config['FHIR_SERVER_URL'] = os.environ.get("FHIR_SERVER_URL")
+    app.config['FHIR_AUTH_SERVER_URL'] = os.environ.get("FHIR_AUTH_SERVER_URL")
+    from cstr.api import root_api
+    app.register_blueprint(root_api)
     return app
-
-@app.route('/api/patient/jim/')
-def get_patient_history():
-    return request.args.get('patient')
-
-@app.route('/api/test')
-def hello_world():
-    return "hello world"
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug = True)
