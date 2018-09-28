3dc1c03174073f40042be4abe60cc84a80e31be5
Greg Gan <ggreg96@gmail.com>
2018-09-28 21:42:12 +1000

    added JSON decoder for reading in request, modified server set up and started on grabbing patients vital

--- cstr/Flask_back.py
+++ cstr/Flask_back.py
@@ -8,8 +8,18 @@ Function to grab patients' data through their ID, from the
 
 @Return: A json file of the request 
 """
-@app.route('/patient/<int:patient_id>', methods=['GET'])
+@app.route('/fhir/Patient/<patient_id>', methods=['GET'])
 def get_patient_history():
+    if request.method == 'GET':
+        return request.args.get('patient_id')
+
+"""
+Function to grab patients' data through their ID, from the 
+
+@Return: A json file of the request 
+"""
+@app.route('/fhir/Patient/<patient_id>', methods=['GET'])
+def get_patient_vitals():
     if request.method == 'GET':
         return request.args.get('patient_id')
 
@@ -22,7 +32,10 @@ Function to read JSON given from GET request from FHIR server.
 def read_json(json_request_return):
     if json_request_return is not None:
         return json.loads(json_request_return)
+    else:
+        return "JSON file is invalid"
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='52.65.117.202', port=8092)
+    #host and port for the FHIR Server
+    app.run(host='52.65.117.202', port=8085)
