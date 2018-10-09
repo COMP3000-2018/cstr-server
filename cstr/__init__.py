d0f7bed81ce9cdfb378423d4b9ecb0637448a92e
Greg Gan <ggreg96@gmail.com>
2018-10-09 17:30:30 +1000

    Working getting patients history

--- cstr/__init__.py
+++ cstr/__init__.py
@@ -6,7 +6,7 @@ fhir_config = {
     'api_base': 'http://smartonfhir.aehrc.com:8085/fhir'
 }
 
-def create_app(config=None):
+def create_app(config=None,config_2=None):
     app = Flask(__name__)
     from cstr.api import root_api
     app.register_blueprint(root_api)
--- cstr/api.py
+++ cstr/api.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, Blueprint, current_app, jsonify, abort, redirect, session
-from fhirclient import client
-import fhirclient.models.patient as patient_model
-import json
+from flask import Flask, request, Blueprint, jsonify, abort, redirect, session
 import requests
+import urllib.parse
 import secrets
-import urllib.parse as urlparse
+import json
+import os
 from cstr import fhir_config
 
 root_api = Blueprint('root_api', __name__, url_prefix='/api')
@@ -18,62 +17,52 @@ def test():
     """
     return jsonify({"value": "Hello World"})
 
-@root_api.route('/authorize', methods=['GET'])
+@root_api.route('/authorize')
 def receive_token():
-    if request.method != 'GET':
-        return abort(404)
-    request.args.get('state')
     if request.args.get('state') != session.pop('state'):
         return abort(500)
-    request.args.get('code')
+    params = {
+        "grant_type": "authorization_code",
+        "code": request.args.get('code'),
+        "redirect_uri": "http://cstr.uqcloud.net/api/authorize",
+        "client_id": "CSTR"
+    }
+    response = requests.post(url="http://smartonfhir.aehrc.com:8080/oauth/token", data=params)
+    session['state'] = None
+    session['token'] = response.text
+    return jsonify({"message": "Auth token associated to user"})
 
 
 @root_api.route('/get_token', methods=['GET'])
 def receive_launch_id():
-    fhir_client = client.FHIRClient(settings=fhir_config)
     session['state'] = secrets.token_urlsafe(16)
-    code_response = requests.get("http://smartonfhir.aehrc.com:8080/oauth/authorize", params={
+    params = {
         "response_type": "code",
         "client_id": "CSTR",
-        "redirect_uri": "cstr.uqcloud.net",
+        "redirect_uri": "http://cstr.uqcloud.net/api/authorize",
         "launch": request.args.get('launch'),
-        "scope": "patient/*.*",
+        "scope": "system/*.*",
         "state": session['state'],
         "aud": request.args.get('iss')
-    })
-    assert code_response.status_code == 302
-    token_url = code_response.headers["Location"]
-    token_queries = urlparse.parse_qs(token_url)
-    if token_queries['state'] != session.pop('state'):
-        return abort(500)
-    token_response = requests.post("http://smartonfhir.aehrc.com:8080/oauth/token",
-        params={
-            "grant_type": "authorization_code",
-            "code": token_queries['code'],
-            "redirect_uri": "cstr.uqcloud.net",
-            "client_id": "CSTR"
-        }
-    )
-    session['token_response'] = token_response.json()
-    return '', 200
+    }
+    param_string = "&".join([key + "=" + urllib.parse.quote(params[key], safe="") for key in params])
+    ehr_url = urllib.parse.urlunparse(('http', 'smartonfhir.aehrc.com:8080', '/oauth/authorize', None, param_string, ''))
+    return redirect(ehr_url)
+    #return redirect("http://smartonfhir.aehrc.com:8080/oauth/authorize?response_type=code&client_id=CSTR&redirect_uri=cstr.uqcloud.net&launch=" + request.args.get('launch') +
+    #    "&scope=patient/*.*&state=" + session['state'] + "&aud=" + request.args.get('iss') )
+
 
 # Endpoint: /api/patient/<patient_id>
-@root_api.route('/patient/<string:patient_id>', methods=['GET'])
-def get_patient_history(patient_id: str):
+@root_api.route('/Patient/<string:patient_id>', methods=['GET'])
+def get_patient_history(patient_id):
     """Endpoint to get patient info from Smart on FHIR server
 
-    @Return: A json file of the request 
+    @Return: A json file of the request
     """
-    if session['token_response'] is None:
-        receive_launch_id()
-    urlparse
-    requests.get("http://smartonfhir.aehrc.com:8085/fhir")
+    if session['token'] is None:
+        abort(401)
+    dict_headers = {"Authorization":"Bearer "+json.loads(session['token'])['access_token']}
+    patient_info = requests.get("http://smartonfhir.aehrc.com:8085/fhir/Patient/" + urllib.parse.quote(patient_id, safe=""),headers=dict_headers)
+    test = json.loads(patient_info.text)
+    return jsonify(test)
 
-def read_json(json_request_return):
-    """Function to read JSON given from GET request from FHIR server.
-
-    @Param: takes a json request
-    @Return: Dictionary of decoded json
-    """
-    if json_request_return is not None:
-        return json.loads(json_request_return)
